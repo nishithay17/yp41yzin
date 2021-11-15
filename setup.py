9850604e71d92e9f79034ddeca071f837e121803
nishithay17 <94312114+nishithay17@users.noreply.github.com>
2021-11-15 08:22:06 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+from setuptools import find_packages
+setup(name='yp41yzin',
+version='0.1',
+author='DSSS',
+author_email='nishitha.yedalapalli@fau.de',
+packages=find_packages(),
+install_requires=['numpy', 'Pillow', 'ipywidgets'])
